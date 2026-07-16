7c9264df50e1232349fe6382d255cd9b11db090d
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "cdn_frontdoor_secrets_id" {
   description = "Map of id values across all cdn_frontdoor_secrets, keyed the same as var.cdn_frontdoor_secrets"
-  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.id }
+  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "cdn_frontdoor_secrets_cdn_frontdoor_profile_id" {
   description = "Map of cdn_frontdoor_profile_id values across all cdn_frontdoor_secrets, keyed the same as var.cdn_frontdoor_secrets"
-  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.cdn_frontdoor_profile_id }
+  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.cdn_frontdoor_profile_id if v.cdn_frontdoor_profile_id != null && length(v.cdn_frontdoor_profile_id) > 0 }
 }
 output "cdn_frontdoor_secrets_cdn_frontdoor_profile_name" {
   description = "Map of cdn_frontdoor_profile_name values across all cdn_frontdoor_secrets, keyed the same as var.cdn_frontdoor_secrets"
-  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.cdn_frontdoor_profile_name }
+  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.cdn_frontdoor_profile_name if v.cdn_frontdoor_profile_name != null && length(v.cdn_frontdoor_profile_name) > 0 }
 }
 output "cdn_frontdoor_secrets_name" {
   description = "Map of name values across all cdn_frontdoor_secrets, keyed the same as var.cdn_frontdoor_secrets"
-  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.name }
+  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "cdn_frontdoor_secrets_secret" {
   description = "Map of secret values across all cdn_frontdoor_secrets, keyed the same as var.cdn_frontdoor_secrets"
-  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.secret }
+  value       = { for k, v in azurerm_cdn_frontdoor_secret.cdn_frontdoor_secrets : k => v.secret if v.secret != null && length(v.secret) > 0 }
 }
 
